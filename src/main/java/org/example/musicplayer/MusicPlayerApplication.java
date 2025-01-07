d6a6d425bb52bb39cf4a538f20cc126772667ee6
Niklas <jan-niklas-bremer@web.de>
2025-01-07 12:26:22 +0100

    Tried applying color to the UI

--- src/main/java/org/example/musicplayer/MusicPlayerApplication.java
+++ src/main/java/org/example/musicplayer/MusicPlayerApplication.java
@@ -11,7 +11,7 @@ public class MusicPlayerApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MusicPlayerApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -19,6 +19,6 @@ public class MusicPlayerApplication extends Application {
 
     public static void main(String[] args) {
         launch();
-        private char hello = 'c';
+        //private char hello = 'c';
     }
 }
