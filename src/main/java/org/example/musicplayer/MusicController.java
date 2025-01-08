52bc7449200319ba2f18b345010e6361485e7a78
deiga <majdei01@easv365.dk>
2025-01-08 10:48:08 +0100

    Added fxid and @FXML to all controls in UI

--- src/main/java/org/example/musicplayer/MusicController.java
+++ src/main/java/org/example/musicplayer/MusicController.java
@@ -2,10 +2,10 @@ package org.example.musicplayer;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 
 import javax.print.attribute.standard.Media;
+import javax.swing.text.html.ImageView;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
@@ -15,9 +15,19 @@ public class MusicController implements Initializable {
 
     @FXML
     private Label welcomeText;
-
     @FXML
     private TextArea songTitle, songView;
+    @FXML
+    private Button buttonPlay, buttonPause, buttonStop, buttonPreviousSong, buttonNextSong, buttonShuffle;
+    @FXML
+    private MenuButton menuButton;
+    @FXML
+    private ComboBox searchBox;
+    @FXML
+    private ImageView pictureFrame;
+    @FXML
+    private ProgressBar progressBar;
+
 
     private Media media;
 
@@ -27,6 +37,12 @@ public class MusicController implements Initializable {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    /**
+     * This method ....
+     * @param url
+     * @param resourceBundle
+     */
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
