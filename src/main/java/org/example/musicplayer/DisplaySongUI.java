fdde4253f1ea6de4ade904dcd225fc739a93ca4f
Sunisima <lsun8525@gmail.com>
2025-01-08 10:26:41 +0100

    Trying to display songs in UI

--- src/main/java/org/example/musicplayer/DisplaySongUI.java
+++ src/main/java/org/example/musicplayer/DisplaySongUI.java
@@ -3,4 +3,5 @@ package org.example.musicplayer;
 public class DisplaySongUI {
 
 
+
 }
--- src/main/java/org/example/musicplayer/MusicController.java
+++ src/main/java/org/example/musicplayer/MusicController.java
@@ -1,23 +1,53 @@
 package org.example.musicplayer;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
-public class MusicController {
+import javax.print.attribute.standard.Media;
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class MusicController implements Initializable {
+
     @FXML
     private Label welcomeText;
 
     @FXML
-    private TextArea songLengthArea;
+    private TextArea songTitle, songView;
+
+    private Media media;
+
+    private int songNumber;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 
-    @FXML
-    public void initialize() {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        File musicDirectory;
+        File[] songFiles;
+
+        ArrayList<File> songs;
+
+        songs = new ArrayList<File>();
+
+        musicDirectory = new File("Music");
+        //Gets all the Music numbers within our music directory map
+        songFiles = musicDirectory.listFiles();
+
+        if (songFiles != null) {
+            for (File file : songFiles) {
+                songs.add(file);
+                //System.out.println(file);
+            }
+        }
+
 
 
     }
