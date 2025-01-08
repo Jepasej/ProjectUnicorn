4aa6ac8311be054482957e7781ef52cf6d1e2e0f
Sunisima <lsun8525@gmail.com>
2025-01-08 08:53:30 +0100

    Created a DisplaySongUI class

---
+++ src/main/java/org/example/musicplayer/DisplaySongUI.java
@@ -0,0 +1,6 @@
+package org.example.musicplayer;
+
+public class DisplaySongUI {
+
+
+}
--- src/main/java/org/example/musicplayer/MusicController.java
+++ src/main/java/org/example/musicplayer/MusicController.java
@@ -2,13 +2,23 @@ package org.example.musicplayer;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class MusicController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private TextArea songLengthArea;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void initialize() {
+
+
     }
 }
