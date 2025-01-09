ad3042e55e11572b8854916f4f5129229f0d82b3
Sunisima <lsun8525@gmail.com>
2025-01-09 11:32:46 +0100

    Getting data from database

--- src/main/java/org/example/musicplayer/MusicController.java
+++ src/main/java/org/example/musicplayer/MusicController.java
@@ -45,7 +45,5 @@ public class MusicController implements Initializable {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        // Hent sange fra databasen og vis i ListView
-        loadSongsFromDatabase();
     }
 }
