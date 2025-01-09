a791d4bc7e93243d4aedde1a772d55535b2cc0e4
deiga <majdei01@easv365.dk>
2025-01-09 09:28:49 +0100

    Connect fxml to controller

--- src/main/java/org/example/musicplayer/MusicController.java
+++ src/main/java/org/example/musicplayer/MusicController.java
@@ -2,8 +2,10 @@ package org.example.musicplayer;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 
+import javax.print.attribute.standard.Media;
+import javax.swing.text.html.ImageView;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -13,7 +15,25 @@ import java.util.ResourceBundle;
 public class MusicController implements Initializable {
 
     @FXML
-    private ListView<String> songList; // Linket til din ListView fra FXML
+    private Label welcomeText;
+    @FXML
+    private TextArea songTitle;
+    @FXML
+    private ListView<String> songList;
+    @FXML
+    private Button buttonPlay, buttonPause, buttonStop, buttonPreviousSong, buttonNextSong, buttonShuffle;
+    @FXML
+    private MenuButton menuButton;
+    @FXML
+    private ComboBox searchBox;
+    @FXML
+    private ImageView pictureFrame;
+    @FXML
+    private ProgressBar progressBar;
+
+
+    private Media media;
+    private int songNumber;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
