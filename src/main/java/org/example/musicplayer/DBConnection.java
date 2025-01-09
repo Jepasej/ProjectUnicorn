0e67fd7b84b4c352906e0659251e752f925ae0ac
Sunisima <lsun8525@gmail.com>
2025-01-09 07:46:08 +0100

    Trying to read data from the DB

--- src/main/java/org/example/musicplayer/DBConnection.java
+++ src/main/java/org/example/musicplayer/DBConnection.java
@@ -1,4 +1,5 @@
 package org.example.musicplayer;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -8,17 +9,23 @@ import java.sql.DriverManager;
  * and establish connection to the DB
  */
 public class DBConnection {
-        //initializes variables for the getConnection method.
-        static final String URL = "jdbc:sqlserver://localhost;portNumber=1433;databaseName=DbMusicPlayer";
-        static final String USERNAME = "sa"; // replace with your username
-        static final String PASSWORD = "admin"; // replace with your password
+    //initializes variables for the getConnection method.
+    static final String URL = "jdbc:sqlserver://localhost;portNumber=1433;databaseName=DbMusicPlayer";
+    static final String USERNAME = "sa"; // replace with your username
+    static final String PASSWORD = "admin"; // replace with your password
 
-        //connects us to the SQL server named DBHundepension
-        public static Connection getConnection() throws Exception {
-            Connection conn = null;
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            return conn;
-        }
+    //connects us to the SQL server named DbMusicPlayer
+    public static Connection getConnection() throws Exception {
+        Connection conn = null;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        return conn;
+    }
+
+    public static void dbConnection(String args[]) throws Exception {
+        //Connects us to the SQL server, when we start the program.
+        System.out.println("Connecting to server.");
+        Connection conn = getConnection();
+    }
 }
 
--- src/main/java/org/example/musicplayer/DisplaySongUI.java
+++ src/main/java/org/example/musicplayer/DisplaySongUI.java
@@ -1,16 +1,32 @@
 package org.example.musicplayer;
 
-
-import java.sql.Connection;
-import java.sql.DriverManager;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.sql.*;
 
 public class DisplaySongUI {
+
     private int songID;
     private String songTitle;
-    private String songArtist;
-    private String albumTitle;
+    private int songLength;
+    private String artist;
+    private String album;
 
+    public static ObservableList<String> displaySongInfo() throws Exception {
+        ObservableList<String> songInfo = FXCollections.observableArrayList();
+        String query = "SELECT fldSongID, fldName, fldLengthInSeconds, fldArtist, fldAlbum FROM tblSongs";
 
+        try (Connection conn = DBConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet result = stmt.executeQuery(query)) {
 
+            while (result.next()) {
+                int songID = Integer.parseInt(result.getString("fldSongID"));
+                String songTitle = result.getString("fldName");
 
+                songInfo.add(songID + " - " + songTitle);
+            }
+        }
+        return songInfo;
+    }
 }
